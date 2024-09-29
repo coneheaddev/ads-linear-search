57b7c1ca8e990cb7a140a1d5d840a4b0310bd854
James Lockett <jamesllockett@gmail.com>
2024-09-29 16:27:41 +0100

    Lineary Search Algorithm

---
+++ README.md
@@ -0,0 +1,8 @@
+# Algorithms & Data Structures
+
+## Linear Search
+
+One of the simplest search algorithms.  This repository contains the code created as part of the YouTube video discussing the algorithm.
+
+
+https://youtu.be/npu55VI4Aog
---
+++ src/LinearSearch/Program.cs
@@ -0,0 +1,23 @@
+﻿// Linear Search
+int[] data = { 1, 2, 3, 4, 5 };
+
+int result_1 = LinearSearch(data, 5);
+int result_2 = LinearSearch(data, 1);
+int result_3 = LinearSearch(data, 6);
+
+Console.WriteLine($"Found 5 at index {result_1}");
+Console.WriteLine($"Found 1 at index {result_2}");
+Console.WriteLine($"Found 6 at index {result_3}");
+
+static int LinearSearch(int[] data, int find)
+{
+    for (int i = 0; i < data.Length; i++)
+    {
+        if (data[i] == find)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
